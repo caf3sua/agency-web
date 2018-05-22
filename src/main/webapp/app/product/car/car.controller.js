6e5b74162e975b511932ae126bf4abf7a6ae1302
Le Viet Dung <dunglv@itsol.vn>
2018-05-22 17:52:01 +0700

    - Update

--- src/main/webapp/app/product/car/car.controller.js
+++ src/main/webapp/app/product/car/car.controller.js
@@ -21,23 +21,52 @@
   		
   		// Properties & function declare
   		vm.productCar = {};
-  		vm.enableOptions = enableOptions;
+  		vm.showChangePremium = showChangePremium;
+  		vm.disableExtendedInsurance = disableExtendedInsurance;
+  		vm.tndsSoChoOptions = [
+  	      {id: '1', name: 'Loại xe dưới 6 chỗ ngồi'},
+  	      {id: '2', name: 'Loại xe từ 6 đến 11 chỗ ngồi'},
+  	      {id: '3', name: 'Loại xe từ 12 đến 24 chỗ ngồi'},
+  	      {id: '4', name: 'Loại xe trên 24 chỗ ngồi'},
+  	      {id: '5', name: 'Xe vừa chở người vừa chở hàng(Pickup, Minivan)'}
+  	    ];
+  		vm.tndstnSoTienOptions = [
+	      {id: '50000000', name: '50trđ/người/vụ về người - 50trđ/vụ về tài sản'},
+	      {id: '100000000', name: '100trđ/người/vụ về người - 100trđ/vụ về tài sản'},
+	      {id: '150000000', name: '150trđ/người/vụ về người - 150trđ/vụ về tài sản'}
+	    ];
+  		vm.nntxSoTienOptions = [
+  	      {id: '10000000', name: '10 triệu đồng/người/vụ'},
+  	      {id: '20000000', name: '20 triệu đồng/người/vụ'},
+  	      {id: '30000000', name: '30 triệu đồng/người/vụ'},
+  	      {id: '40000000', name: '40 triệu đồng/người/vụ'},
+  	      {id: '50000000', name: '50 triệu đồng/người/vụ'},
+  	      {id: '100000000', name: '100 triệu đồng/người/vụ'},
+  	      {id: '150000000', name: '150 triệu đồng/người/vụ'},
+  	      {id: '200000000', name: '200 triệu đồng/người/vụ'}
+  	    ];
+  		vm.manufacturerOptions = [];
+  		vm.modelOptions = [];
+  		vm.yearOptions = [];
   		
   		
   		// Function
-  		function enableOptions(checkBox) {
-  			console.log(vm.productCar)
-  			switch(checkBox.id) {
-  				case 'INSURANCE_TARGET':
-  					break;
-  				case 'INSURANCE_TYPE_FORCE':
-  					break;
-  				case 'INSURANCE_TYPE_VOLUNTEER':
-  					break;
-  				case 'INSURANCE_TYPE_ACCIDENT':
-  					break;
-  				case 'INSURANCE_TYPE_RESOURCE':
-  					break;
+  		function showChangePremium() {
+  			if((vm.productCar.tndsbbCheck && vm.productCar.tndsSoCho)
+  					|| (vm.productCar.tndstnCheck && vm.productCar.tndsSoCho)) {
+  				return true;
+  			} else {
+  				return false;
+  			}
+  		}
+  		
+  		function disableExtendedInsurance() {
+  			if((!vm.productCar.tndsbbCheck && !vm.productCar.tndstnCheck) 
+  					|| (vm.productCar.tndsbbCheck && !vm.productCar.tndsSoCho)
+  					|| (vm.productCar.tndstnCheck && !vm.productCar.tndsSoCho)) {
+  				return true;
+  			} else {
+  				return false;
   			}
   		}
     }
