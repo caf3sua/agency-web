b09167c0460debd438cd9567ba4d67ba1cb3b9fc
dangthehungitsol <dangthehungitsol@itsol-HungDT.bvgroup.baoviet.vn>
2018-05-22 16:00:16 +0700

    refactor menu

---
+++ src/main/webapp/app/contact/contact.state.js
@@ -0,0 +1,32 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('pteMagicApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider) {
+        $stateProvider.state('contact', {
+            parent: 'app',
+            url: '/contact',
+            data: {
+                authorities: []
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/contact/contact.html',
+                    controller: 'ContactController',
+                    controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate,$translatePartialLoader) {
+                    $translatePartialLoader.addPart('home');
+                    return $translate.refresh();
+                }]
+            }
+        });
+    }
+})();
