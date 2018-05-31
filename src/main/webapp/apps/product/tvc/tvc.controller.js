271c811cdeb8bb24d029ed0a300d44da00166f64
phuongna <phuongna@itsol.vn>
2018-05-31 13:43:11 +0700

    fix

--- src/main/webapp/apps/product/tvc/tvc.controller.js
+++ src/main/webapp/apps/product/tvc/tvc.controller.js
@@ -49,7 +49,9 @@
         vm.getPolicyNumber = getPolicyNumber;
         vm.infoPerson = infoPerson;
         vm.onchangeReceiveMethod = false;
-        vm.createNewPolicy = createNewPolicy;
+        vm.createNewPolicy = createNewPolicy;4
+        vm.showChangePremium = showChangePremium;
+        vm.isShowChangePremium = false;
         angular.element(document).ready(function () {
         });
 
@@ -60,7 +62,13 @@
             getPolicyNumber();
 
   		})();
-
+        function showChangePremium() {
+            if(vm.tvcBaseVM.destinationId != "") {
+                return true;
+            } else {
+                return false;
+            }
+        }
   		// Properties & function declare
 
         function getPolicyNumber() {
@@ -79,20 +87,34 @@
         }
         function getPremium() {
             vm.premiumTvcVM.destination =  vm.tvcBaseVM.destinationId;
-            vm.premiumTvcVM.ngayDi =  vm.tvcBaseVM.ngayDi;
-            vm.premiumTvcVM.ngayVe  =  vm.tvcBaseVM.ngayVe;
+            vm.premiumTvcVM.ngayDi =  vm.tvcBaseVM.inceptionDate;
+            vm.premiumTvcVM.ngayVe  =  vm.tvcBaseVM.expiredDate;
             vm.premiumTvcVM.numberOfPerson  =  1;
             vm.premiumTvcVM.planId  =  vm.tvcBaseVM.planId;
-            vm.premiumTvcVM.premiumDiscount  =  0;
             vm.premiumTvcVM.premiumNet  =  0;
             vm.premiumTvcVM.premiumPackage  =  vm.tvcBaseVM.travelWithId;
             vm.premiumTvcVM.premiumTvc  = 0;
+            if(vm.premiumTvcVM.premiumDiscount > 0 ){
+                vm.premiumTvcVM.premiumDiscount = vm.premiumTvcVM.premiumDiscount;
+            }else{
+                vm.premiumTvcVM.premiumDiscount  = 0;
+            }
             vm.premiumTvcVM.songay  = 0;
             TvcService.getPremium(vm.premiumTvcVM, onGetPremiumSuccess, onGetPremiumError);
         }
         function onGetPremiumSuccess(result) {
-            vm.tvcBaseVM.premiumTvc = result.premiumTvc;
+            vm.tvcBaseVM.premium  = result.premiumTvc;
             vm.tvcBaseVM.soNguoiThamGia  = result.numberOfPerson;
+            vm.tvcBaseVM.netPremium   = result.premiumNet;
+            vm.tvcBaseVM.changePremium  = result.premiumDiscount;
+            if(result.premiumDiscount > 0){
+                vm.isShowChangePremium = true;
+                vm.sumPremiumDiscount =  vm.tvcBaseVM.netPremium - vm.tvcBaseVM.premium ;
+            }
+
+
+            // "premiumTvc": 104500,
+            //     "premiumNet": 110000,sumPremiumDiscount
         }
 
         function onGetPremiumError(result) {
@@ -100,6 +122,7 @@
         }
         function infoPerson() {
             vm.tvcBaseVM.listTvcAddBaseVM.push(vm.tvcAddBaseVM);
+            vm.tvcAddBaseVM = {};
             console.log(vm.tvcBaseVM);
         }
         function createNewPolicy() {
@@ -108,6 +131,23 @@
             }else{
                 vm.tvcBaseVM.receiveMethod = '2';
             }
+            vm.tvcBaseVM.invoiceInfo.name = vm.invoiceInfoData.name;
+            vm.tvcBaseVM.invoiceInfo.company = vm.invoiceInfoData.company;
+            vm.tvcBaseVM.invoiceInfo.taxNo = vm.invoiceInfoData.taxNo;
+            vm.tvcBaseVM.invoiceInfo.address = vm.invoiceInfoData.address;
+            vm.tvcBaseVM.invoiceInfo.accountNo = vm.invoiceInfoData.accountNo;
+            vm.tvcBaseVM.receiverUser.name  = vm.receiverUserData.name;
+            vm.tvcBaseVM.receiverUser.address  = vm.receiverUserData.address;
+            vm.tvcBaseVM.receiverUser.addressDistrict  = vm.receiverUserData.addressDistrict;
+            vm.tvcBaseVM.receiverUser.mobile  = vm.receiverUserData.mobile;
+            vm.tvcBaseVM.receiverUser.email  = vm.receiverUserData.email;
+            TvcService.createNewPolicy(vm.tvcBaseVM, onGetCreateSuccess, onGetCreateError);
+        }
+        function onGetCreateSuccess(result) {
+        }
+
+        function onGetCreateError(result) {
+            toastr.error('Get data error!', 'Error');
         }
     }
 })();
