5da8473d8eb95320303b6831e1e9e9575d5ea5dd
phuongna <phuongna@itsol.vn>
2018-06-18 09:05:09 +0700

    fix

--- src/main/webapp/apps/cart/cart.controller.js
+++ src/main/webapp/apps/cart/cart.controller.js
@@ -10,12 +10,16 @@
     function CartController ($scope, Principal, $state, $rootScope, CartService) {
     	var vm = this;
         vm.getAllOrder = getAllOrder;
+        vm.newDate = null;
+        vm.cartWarning = false;
+        vm.cartCheckBox = false;
         angular.element(document).ready(function () {
         });
 
         // Init controller
         (function initController() {
             getAllOrder();
+            vm.newDate = new Date();
         })();
 
   		// Properties & function declare
@@ -27,6 +31,13 @@
         }
         function onGetAllOrderSuccess(result) {
             vm.allOrder = result;
+            for (var i = 0; i < vm.allOrder.length; i++) {
+                if( vm.allOrder[i].inceptionDate < vm.newDate){
+                    vm.cartWarning = true;
+                }else{
+                    vm.cartCheckBox = true;
+                }
+            }
         }
         function onGetAllOrderError() {
             toastr.error("error!");
