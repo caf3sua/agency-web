65b28225e9dd7059ba4f3a269b29c527ebccac70
trinhminhthao_itsol <trinhminhthao_itsol@itsol-HungDT.bvgroup.baoviet.vn>
2018-06-20 10:22:08 +0700

    add quick cal

--- src/main/webapp/apps/nav/nav.controller.js
+++ src/main/webapp/apps/nav/nav.controller.js
@@ -12,7 +12,22 @@
     		, $timeout, $window, Auth, $state, Principal, ContactCommonDialogService, ReminderService, CONSTANT_REMINDER_RANGER_DATE) {
     	var vm = this;
     	vm.countReminder = 0;
-    	vm.isCanPremium = true;
+    	vm.isCanPremium = false;
+    	
+    	vm.products = [
+    		{"lineId": "", "lineName" : "-- Chọn sản phẩm --"}, 
+    		{"lineId": "CAR", "lineName" : "Bảo hiểm ô tô"}, 
+    		{"lineId": "BVP", "lineName" : "Bảo Việt An Gia"},
+    		{"lineId": "KCARE", "lineName" : "Bảo hiểm bệnh ung thu"},
+    		{"lineId": "TVC", "lineName" : "Bảo hiểm du lịch quốc tế"},
+    		{"lineId": "TVI", "lineName" : "Bảo hiểm du lịch Việt Nam"},
+    		{"lineId": "MOTO", "lineName" : "Bảo hiểm xe máy"},
+    		{"lineId": "HOME", "lineName" : "Bảo hiểm nhà tư nhân"},
+    		{"lineId": "KHC", "lineName" : "Bảo hiểm kết hợp con người"},
+    		{"lineId": "TNC", "lineName" : "Bảo hiểm tai nạn con người"},
+    		{"lineId": "HHVC", "lineName" : "Bảo hiểm vận chuyển hàng hóa"}
+    	];
+    	vm.selProduct = "";
     	
     	vm.logout = logout;
 
