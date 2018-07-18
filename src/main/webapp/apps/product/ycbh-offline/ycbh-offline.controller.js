32822c95d308695d9c993021c1e832ba5bf67a98
caf3sua <caf3sua@gmail.com>
2018-07-18 09:50:24 +0700

    fix

--- src/main/webapp/apps/product/ycbh-offline/ycbh-offline.controller.js
+++ src/main/webapp/apps/product/ycbh-offline/ycbh-offline.controller.js
@@ -13,6 +13,10 @@
         vm.policy = {};
         
         // Properties & function declare
+        vm.uploadGycbh = uploadGycbh;
+        vm.uploadImgGiamdinh = uploadImgGiamdinh;
+        vm.uploadOtherDoc = uploadOtherDoc;
+        vm.uploadAnchi = uploadAnchi;
         
         angular.element(document).ready(function () {
         });
@@ -27,5 +31,59 @@
   		})();
   		
   		// Function
+  		
+  		function uploadGycbh(file, errFiles) {
+//            vm.f = file;
+//            vm.errFile = errFiles && errFiles[0];
+        	// validate
+            if (file) {
+            	var fileReader = new FileReader();
+            	fileReader.readAsDataURL(file);
+            	fileReader.onload = function (e) {
+            	  var dataUrl = e.target.result;
+            	  var base64Data = dataUrl.substr(dataUrl.indexOf('base64,') + 'base64,'.length);
+            	  console.log(base64Data);
+            	};
+            }
+        }
+  		
+  		function uploadImgGiamdinh(file, errFiles) {
+        	// validate
+            if (file) {
+            	var fileReader = new FileReader();
+            	fileReader.readAsDataURL(file);
+            	fileReader.onload = function (e) {
+            	  var dataUrl = e.target.result;
+            	  var base64Data = dataUrl.substr(dataUrl.indexOf('base64,') + 'base64,'.length);
+            	  console.log(base64Data);
+            	};
+            }
+        }
+  		
+  		function uploadOtherDoc(file, errFiles) {
+        	// validate
+            if (file) {
+            	var fileReader = new FileReader();
+            	fileReader.readAsDataURL(file);
+            	fileReader.onload = function (e) {
+            	  var dataUrl = e.target.result;
+            	  var base64Data = dataUrl.substr(dataUrl.indexOf('base64,') + 'base64,'.length);
+            	  console.log(base64Data);
+            	};
+            }
+        }
+  		
+  		function uploadAnchi(file, errFiles) {
+        	// validate
+            if (file) {
+            	var fileReader = new FileReader();
+            	fileReader.readAsDataURL(file);
+            	fileReader.onload = function (e) {
+            	  var dataUrl = e.target.result;
+            	  var base64Data = dataUrl.substr(dataUrl.indexOf('base64,') + 'base64,'.length);
+            	  console.log(base64Data);
+            	};
+            }
+        }
     }
 })();
