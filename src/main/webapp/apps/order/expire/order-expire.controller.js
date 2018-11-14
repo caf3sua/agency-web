a30d506d6101249932a8f28740fb541b9c7bae09
Le Minh Duc <duclm2406@gmail.com>
2018-11-14 16:33:09 +0700

    update

---
+++ src/main/webapp/apps/order/expire/order-expire.controller.js
@@ -0,0 +1,131 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('OrderExpireController', OrderExpireController);
+
+    OrderExpireController.$inject = ['$scope', '$stateParams', '$controller', '$state', '$rootScope', 'OrderService', '$ngConfirm', '$timeout', 'PAGINATION_CONSTANTS', '$uibModal'];
+
+    function OrderExpireController ($scope, $stateParams, $controller, $state, $rootScope, OrderService, $ngConfirm, $timeout, PAGINATION_CONSTANTS, $uibModal) {
+    	var vm = this;
+        
+    	// Properties & function declare
+        // paging
+    	vm.page = 1;
+        vm.totalItems = null;
+        vm.itemsPerPage = PAGINATION_CONSTANTS.itemsPerPage;
+        vm.transition = transition;
+        
+  		vm.isLoading = false;
+  		vm.orders = [];
+  		vm.searchCriterial = {
+			  "pageable": {
+			    "page": 0,
+			    "size": vm.itemsPerPage
+			  },
+  			  "contactCode": "",
+  			  "contactName": "",
+  			  "email": "",
+  			  "fromDate": "",
+  			  "gycbhNumber": "",
+  			  "statusPolicy": "",
+  			  "phone": "",
+  			  "productCode": "",
+  			  "toDate": "",
+  			  "createType": ""
+  		};
+  		vm.sotiennophi;
+  		
+  		vm.searchOrderExpire = searchOrderExpire;
+  		vm.confirmViewAgreement = confirmViewAgreement;
+  		vm.changeDate = changeDate;
+  		
+        angular.element(document).ready(function () {
+        });
+
+    	// Init controller
+  		(function initController() {
+  			// instantiate base controller
+//  			$controller('ProductBaseController', { vm: vm, $scope: $scope });
+  			
+  		    $controller('AgreementBaseController', { vm: vm, $scope: $scope });
+  		   
+  		    searchOrderExpire();
+  		})();
+  		
+  		function changeDate(){
+  			if (vm.searchCriterial.fromDate != "" && vm.searchCriterial.toDate != ""){
+  				if(!vm.checkDate(vm.searchCriterial.fromDate, vm.searchCriterial.toDate)){
+  					toastr.error("Thời gian từ ngày - đến ngày không phù hợp");
+  					return false;
+  				}
+  			}
+  			return true;
+  		}
+  		
+  		function searchOrderExpire() {
+  			if (changeDate()) {
+  				vm.totalItems = null;
+  	  			vm.isLoading = true;
+  	  			vm.orders = [];
+  	  			var order = {};
+
+  	  			debugger
+  	  			OrderService.searchOrderExpire(vm.searchCriterial, onSearchSuccess, onSearchError);
+  	  			function onSearchSuccess(result, headers) {
+  	  				// Paging
+  	  				vm.orders = result;
+  	  				vm.isLoading = false;
+  	                
+  	  				vm.totalItems = headers('X-Total-Count');
+  	                vm.queryCount = vm.totalItems;
+  	                
+  	  				toastr.success('Tìm thấy ' + vm.orders.length + ' đơn hàng phù hợp');
+  	  	        }
+  	  	        function onSearchError() {
+  	  	        	vm.isLoading = false;
+  	  	            toastr.error("Lỗi khi tìm kiếm đơn hàng!");
+  	  	        }
+  			}
+  		}
+  		
+  		function search(page) {
+  			console.log('transition, page:' + vm.page);
+  			vm.isLoading = true;
+
+  			var order = {};
+  			order = vm.searchCriterial;
+  			order.pageable.page = vm.page - 1;
+        	console.log('searchAllTransition, page: ' + order.pageable.page);
+        	
+  			OrderService.searchOrderExpire(order, onSearchSuccess, onSearchError);
+  			function onSearchSuccess(result, headers) {
+  				// Paging
+  				vm.orders = result;
+  				vm.isLoading = false;
+  				toastr.success('Tìm thấy ' + vm.orders.length + ' đơn hàng phù hợp');
+  	        }
+  	        function onSearchError() {
+  	        	vm.isLoading = false;
+  	            toastr.error("Lỗi khi tìm kiếm đơn hàng!");
+  	        }
+  		}
+  		
+        function transition () {
+        	// search
+        	search();
+        }
+        
+  		function confirmViewAgreement(order) {
+  			if (order.createType == "0"){
+  				$state.go("order.order-detail", {id: order.agreementId});
+  			} else if (order.createType == "2") {
+  				$state.go("product.printed-paper-detail", {id: order.gycbhNumber});
+  			} else {
+  				$state.go("product.ycbh-offline-detail", {id: order.gycbhNumber});
+  			}
+  		}
+  		
+    }
+})();
