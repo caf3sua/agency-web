4ca0b39bb2a900459f5a7e4e25b5644a6f4b25ae
caf3sua <caf3sua@gmail.com>
2018-11-14 16:22:43 +0700

    add search theo phong ban

--- src/main/webapp/apps/agreement-base.controller.js
+++ src/main/webapp/apps/agreement-base.controller.js
@@ -8,10 +8,10 @@
       .controller('AgreementBaseController', AgreementBaseController);
 
     AgreementBaseController.$inject = ['vm', '$state', '$stateParams', '$rootScope', '$scope', '$timeout'
-    	, '$ngConfirm', 'OrderService'];
+    	, '$ngConfirm', 'OrderService', 'Principal'];
 
     function AgreementBaseController(vm, $state, $stateParams, $rootScope, $scope, $timeout
-    		, $ngConfirm, OrderService){
+    		, $ngConfirm, OrderService, Principal){
 		vm.message = { name: 'default entry from AgreementBaseController' };
 
 		vm.confirmCancelOrder = confirmCancelOrder;
@@ -19,6 +19,19 @@
 		vm.confirmCopyAgreement = confirmCopyAgreement;
 		vm.searchOrder = searchOrder;
 		
+		// Init controller
+  		(function initController() {
+  			// instantiate base controller
+  		    getAccount();
+  		    
+		})();
+		  
+		function getAccount() {
+  			Principal.identity().then(function(account) {
+                vm.currentAccount = account;
+            });
+  		}
+
 		function confirmCopyAgreement(order) {
   			$ngConfirm({
                 title: 'Xác nhận',
