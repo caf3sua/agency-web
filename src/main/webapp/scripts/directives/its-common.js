be159491de99d3a997101036d7cb0849ecb65a22
trinhminhthao_itsol <trinhminhthao_itsol@itsol-HungDT.bvgroup.baoviet.vn>
2018-11-21 17:09:57 +0700

    fix

--- src/main/webapp/scripts/directives/its-common.js
+++ src/main/webapp/scripts/directives/its-common.js
@@ -2,6 +2,7 @@
   'use strict';
   angular
     .module('app')
+    .directive('acceptedCharacter', acceptedCharacter)
     .directive('itsAsideMenu', itsAsideMenu);
 
   	itsAsideMenu.$inject = [];
@@ -10,23 +11,31 @@
         directive.restrict = 'E';
         directive.template = "<li ui-sref-active=\"active\"><a ui-sref=\"app.inbox.list\"><span class=\"nav-text\">Inbox</span></a></li>";
 
-////        directive.scope = {
-////           sinhvien : "=ten"
-////        }
-//		 
-////        directive.compile = function(element, attributes) {
-////           element.css("border", "1px solid #cccccc");
-////
-////           var hamLienKet = function($scope, element, attributes) {
-////              element.html("Sinh vien: <b>"+$scope.sinhvien.ten +"</b> , MSSV: <b>"+$scope.sinhvien.mssv+"</b><br/>");
-////              element.css("background-color", "#ff00ff");
-////           }
-////
-////           return hamLienKet;
-////        }
-
         return directive;
     }
     
+    
+    acceptedCharacter.$inject = [];
+    function acceptedCharacter() {
+      var directive = {
+          require: 'ngModel',
+          link: link
+      };
+      return directive;
+      
+      function link(scope, element, attrs, modelCtrl) {
+    	  modelCtrl.$parsers.push(function (inputValue) {
+    		  /^[a-zA-Z][a-zA-Z\\s]+$/;
+              var transformedInput = inputValue ? inputValue.replace(/[^\a-zA-Z0-9_.-]/g,'') : null;
+
+              if (transformedInput!=inputValue) {
+                  modelCtrl.$setViewValue(transformedInput);
+                  modelCtrl.$render();
+              }
+
+              return transformedInput;
+          });
+      }
+    }
     
 })();
