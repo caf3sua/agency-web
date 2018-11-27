eb329a8763c958d4452b2a5104eed6e832cacf45
leminhduc_itsol <leminhduc_itsol@ITSD1-DUCLM.bvgroup.baoviet.vn>
2018-11-27 12:45:42 +0700

    update search order

--- src/main/webapp/apps/order/me/order.controller.js
+++ src/main/webapp/apps/order/me/order.controller.js
@@ -29,9 +29,7 @@
   			  "email": "",
   			  "fromDate": "",
   			  "gycbhNumber": "",
-  			  "lstStatusPolicy": [
-  			    ""
-  			  ],
+  			  "statusPolicy": "",
   			  "phone": "",
   			  "productCode": "",
   			  "toDate": "",
