fc9956da8cad6b47b4b369a7ee8482334b39d687
leminhduc_itsol <leminhduc_itsol@ITSD1-DUCLM.bvgroup.baoviet.vn>
2018-12-28 16:37:45 +0700

    update

--- src/main/java/com/baoviet/app/ApplicationWeb.java
+++ src/main/java/com/baoviet/app/ApplicationWeb.java
@@ -101,37 +101,37 @@ public class ApplicationWeb extends SpringBootServletInitializer {
         return registrationBean;
     }
 
-	@Bean
-	public EmbeddedServletContainerFactory servletContainer() {
-		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
-
-			@Override
-			protected void postProcessContext(Context context) {
-				SecurityConstraint securityConstraint = new SecurityConstraint();
-				securityConstraint.setUserConstraint("CONFIDENTIAL");
-				SecurityCollection collection = new SecurityCollection();
-				collection.addPattern("/*");
-				securityConstraint.addCollection(collection);
-				context.addConstraint(securityConstraint);
-			}
-		};
-		tomcat.addAdditionalTomcatConnectors(createHttpConnector());
-		return tomcat;
-	}
-
-	@Value("${server.http.port}")
-	private int serverPortHttp;
-
-	@Value("${server.port}")
-	private int serverPortHttps;
-
-	private Connector createHttpConnector() {
-		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-		connector.setScheme("http");
-		connector.setSecure(false);
-		connector.setPort(serverPortHttp);
-		connector.setRedirectPort(serverPortHttps);
-		return connector;
-	}
+//	@Bean
+//	public EmbeddedServletContainerFactory servletContainer() {
+//		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+//
+//			@Override
+//			protected void postProcessContext(Context context) {
+//				SecurityConstraint securityConstraint = new SecurityConstraint();
+//				securityConstraint.setUserConstraint("CONFIDENTIAL");
+//				SecurityCollection collection = new SecurityCollection();
+//				collection.addPattern("/*");
+//				securityConstraint.addCollection(collection);
+//				context.addConstraint(securityConstraint);
+//			}
+//		};
+//		tomcat.addAdditionalTomcatConnectors(createHttpConnector());
+//		return tomcat;
+//	}
+//
+//	@Value("${server.http.port}")
+//	private int serverPortHttp;
+//
+//	@Value("${server.port}")
+//	private int serverPortHttps;
+//
+//	private Connector createHttpConnector() {
+//		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//		connector.setScheme("http");
+//		connector.setSecure(false);
+//		connector.setPort(serverPortHttp);
+//		connector.setRedirectPort(serverPortHttps);
+//		return connector;
+//	}
 }
 
